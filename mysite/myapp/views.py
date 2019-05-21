872093a764281d32b19e1ee7bb661c7a2c3fa9ec
Krzysztof <50321802+1998krzysiek@users.noreply.github.com>
2019-05-21 22:07:11 +0200

    Błąd pętli
    Trzeba było o jeden raz więcej tab zrobić

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, render_to_response
 import random as rm
 # Create your views here.
 def index(request):
-	
-	madrosci=["a","b"]
+
+	madrosci=["j","v"]
 
 	while madrosci:
- 	   losowe = rm.choice(madrosci)
-    	   print(losowe)
-    	   madrosci.remove(losowe)
-	skrypt = {'skrypt':madrosci}
+		losowe=rm.choice(madrosci)
+		c=losowe
+		madrosci.remove(losowe)
+
+
+	skrypt = {'skrypt':c}
 	return render(request,'index.html',skrypt)
