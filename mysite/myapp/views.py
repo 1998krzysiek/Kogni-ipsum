9b16230803bf393259438ee9037307868b3fc280
NataliaSkreczko <50436253+NataliaSkreczko@users.noreply.github.com>
2019-05-25 00:28:20 +0200

    Wyświetlanie formularzy (zmienione)

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render, render_to_response
 import random as rm
-# Create your views here.
+from .forms import ContactForm
+
 def index(request):
+	wynik=""
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			try:
+				name=form.cleaned_data['name']
+				madrosci=["j","v","a","b","c","u","i"]
+				lista=[]
+				for i in range(int(name)):
+					cytat=rm.choice(madrosci)
+					madrosci.remove(cytat)
+					lista.append(cytat)
+				lista=' '.join(lista)
+				wynik=lista
+			except ValueError:
+				wynik=""
+			except IndexError:
+				wynik=""
+	form = ContactForm()
+	return render(request,'index.html', {'form': form,'skrypt':wynik})
+'''
+for i in range(wybor+1):
+	if i==1:
+		None
+	else:
+		print('\n')
 
-	madrosci=["j","v"]
-
-	while madrosci:
-		losowe=rm.choice(madrosci)
-		c=losowe
-		madrosci.remove(losowe)
-
-
-	skrypt = {'skrypt':c}
-	return render(request,'index.html',skrypt)
+	for j in range(0,random.randint(3,15)):
+		cytat=random.choice(madrosci)
+		madrosci.remove(cytat)
+		print(cytat)
+'''
