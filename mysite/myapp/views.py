f9405a9eb4791cb9820764b96cd6633c1cd86034
Alicja0628 <50325263+Alicja0628@users.noreply.github.com>
2019-05-26 23:53:35 +0200

    Forms
    Pomyliłam foldery

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,37 +1,7 @@
-from django.shortcuts import render, render_to_response
-import random as rm
-from .forms import ContactForm
+from django import forms
 
-def index(request):
-	wynik=""
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			try:
-				name=form.cleaned_data['name']
-				madrosci=["j","v","a","b","c","u","i"]
-				lista=[]
-				for i in range(int(name)):
-					cytat=rm.choice(madrosci)
-					madrosci.remove(cytat)
-					lista.append(cytat)
-				lista=' '.join(lista)
-				wynik=lista
-			except ValueError:
-				wynik=""
-			except IndexError:
-				wynik=""
-	form = ContactForm()
-	return render(request,'index.html', {'form': form,'skrypt':wynik})
-'''
-for i in range(wybor+1):
-	if i==1:
-		None
-	else:
-		print('\n')
+class ContactForm(forms.Form):
+	rand = forms.CharField()
 
-	for j in range(0,random.randint(3,15)):
-		cytat=random.choice(madrosci)
-		madrosci.remove(cytat)
-		print(cytat)
-'''
+class ContactForm2(forms.Form):
+	aka = forms.CharField()
